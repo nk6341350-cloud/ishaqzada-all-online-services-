a319dd1d6bc026206f650fb281ed84ced94f1c9e
nk6341350-cloud <nk6341350@gmail.com>
2026-09-19 01:28:29 +0430

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-let deferredPrompt=null;
-window.addEventListener("beforeinstallprompt",e=>{e.preventDefault();deferredPrompt=e});
-document.getElementById("installBtn").addEventListener("click",async()=>{if(deferredPrompt){deferredPrompt.prompt();await deferredPrompt.userChoice;deferredPrompt=null}else alert("د نصب اختیار د موبایل له براوزر څخه وکاروئ.")});
-document.querySelectorAll(".service").forEach(b=>b.addEventListener("click",()=>{alert("دا خدمت به وروسته د خپل اصلي فایل سره وصل شي.")}));
-if("serviceWorker" in navigator) window.addEventListener("load",()=>navigator.serviceWorker.register("sw.js"));
+let deferredPrompt;addEventListener("beforeinstallprompt",e=>{e.preventDefault();deferredPrompt=e});document.querySelector("#install").onclick=async()=>{if(deferredPrompt){deferredPrompt.prompt();await deferredPrompt.userChoice;deferredPrompt=null}else alert("د موبایل د براوزر له نصب اختیار څخه استفاده وکړئ.")};const m=document.querySelector("#soon");document.querySelectorAll(".card").forEach(x=>x.onclick=()=>m.hidden=false);document.querySelector("#close").onclick=()=>m.hidden=true;if("serviceWorker"in navigator)addEventListener("load",()=>navigator.serviceWorker.register("./sw.js"));
